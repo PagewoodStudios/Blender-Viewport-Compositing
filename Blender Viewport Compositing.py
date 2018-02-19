4c52b8656e7f10e8d9758ce654682f327c9d16ce
AdamEarle <MrAdamEarle@gmail.com>
2018-02-19 21:50:51 +1100

    Updated Credits

--- Blender Viewport Compositing.py
+++ Blender Viewport Compositing.py
@@ -1,11 +1,20 @@
 '''
-Overviiew:
+Overview:
 
     Bringing the compositor to the 3D viewport
 
     1.Split screen divider -Nuke/ Fusion like experience
     2.Node tree overlay in the 3D viewport
     3. Handling the dodads of the interface
+
+
+
+
+Credits:
+    Pagewood Studios - www.PagewoodStudios.com
+    Adam Earle
+
+
 
 
 '''
