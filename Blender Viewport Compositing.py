ca4ae494ce6e218e856da89daccddb0a57d31b3e
AdamEarle <MrAdamEarle@gmail.com>
2018-02-19 19:24:40 +1100

    Setup
    Setting up a basic  overview

---
+++ Blender Viewport Compositing.py
@@ -0,0 +1,11 @@
+'''
+OVERVIEW:
+
+    Bringing the compositor to the 3D viewport
+
+    1.Split screen devider -Nuke/ Fusion like experience
+    2.Node tree overlay in the 3D viewport
+    3. Handling the dodads of the interface
+
+
+'''
